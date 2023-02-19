d6c762d32c34c70fda86cd368b4e0bca7fcaf9cf
Cristina <cristy.wolf666@gmail.com>
2023-02-19 19:27:56 +0200

    contact

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import Btn from '../common/subcomp/Btn'
 
 const Form = () => {
+    const [name, setName] = useState("");
+    const [lname, setLName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const [show, setShow] = useState(true);
+
+    const handleChangeName = event => {
+        setName( event.target.value);
+    };
+    const handleChangeLName= event => {
+        setLName( event.target.value);
+    };    
+    const handleChangeEmail = event => {
+        setEmail( event.target.value);
+    };    
+    const handleChangeMessage = event => {
+        setMessage( event.target.value);
+    };
+    const form = useRef();
+
+        const sendEmail = (event) => {
+      console.log(name, lname, email, message);
+      setShow(!show);
+      event.preventDefault();
+
+      emailjs.sendForm('service_sms3izg', 'template_bvxeaeh', form.current, 'cJq7vuqUxvTO9HxJB')
+        .then((result) => {
+            console.log(result.text);
+            console.log("sent")
+        }, (error) => {
+            console.log(error.text);
+            console.log("error")
+        });
+    };
   return (
     <>
-        <form>
+        <form ref={form} onSubmit={sendEmail} className={ show ? 'visible' : 'invisible'}>
         <div className="flex flex-row text-secondary">
             <div className="relative w-6/12 m-2">
                 <input type="text" name="user_fname" value={name} onChange={handleChangeName} id="first_name" className="block py-2 px-0 w-full text-sm text-secondary bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="First name" required />
@@ -27,6 +63,9 @@ const Form = () => {
         </div>
         <div className='flex justify-end'>
             <Btn title={"Submit"}/>
+        </div>
+         <div className={show ? "invisible" : "visible"} id="mess">
+              <h1>Formular trimis cu succes</h1>
         </div>
         </form>
     </>
